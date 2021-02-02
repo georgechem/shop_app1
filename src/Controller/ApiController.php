ab6862eecba99c5e89e13993cff393d91c052235
georgechem <54766527+georgechem@users.noreply.github.com>
2021-02-02 21:04:17 +0000

    api improved with selflink

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -10,7 +10,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class ApiController extends AbstractController
 {
     /**
-     * @Route("/googleBooks", name="app_google_books")
+     * @Route("/googleBooks", name="app_google_books", methods={"GET", "HEAD"})
      */
     public function index(): JsonResponse
     {
@@ -20,14 +20,30 @@ class ApiController extends AbstractController
 
         $service = new \Google_Service_Books($client);
 
-        $query = 'php';
+        $query = 'book';
 
         $optParams = array(
+            'startIndex' => 1,
             'maxResults' => 1,
+            'filter'=>'ebooks'
         );
         $results = $service->volumes->listVolumes($query, $optParams);
+        $data =null;
+        if(true){
+            $data = $results->items[0]->selfLink;
+        }else{
+            $data = $results;
+        }
 
+        $opts = array(
+            'http'=>array(
+                'method'=>"GET",
+            )
+        );
+        $context = stream_context_create($opts);
 
-        return new JsonResponse($results);
+        $test = file_get_contents($data, false, $context);
+
+        return new JsonResponse(json_decode($test));
     }
 }
