77bd53f5d05081605d33a6e7d22b3d320b287887
georgechem <54766527+georgechem@users.noreply.github.com>
2021-02-04 11:41:44 +0000

    googleBooks/1/40/pc

--- public/js/pages/homepage.js
+++ public/js/pages/homepage.js
@@ -46,7 +46,7 @@ let latestRight = document.getElementById('latestRight');
  *
   */
 let getBooks = function(start, total){
-    fetch(`https://localhost:8000/myBooks/${start}/${total}/technology`)
+    fetch(`https://localhost:8000/myBooks/${start}/${total}/pc`)
         .then((response)=>{
             return response.json();
         })
