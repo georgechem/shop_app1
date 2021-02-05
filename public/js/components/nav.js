9802d5c4f1af99dbdc8bc0ad9b0b2227e75e702f
georgechem <54766527+georgechem@users.noreply.github.com>
2021-02-05 20:07:35 +0000

    strange error javascript navigation

--- public/js/components/nav.js
+++ public/js/components/nav.js
@@ -1,32 +1,39 @@
-// Get from DOM Two elements which represents TWO icons
-let navBook = document.getElementById('navBook');
-let navUser = document.getElementById('navUser');
+
+    // Get from DOM Two elements which represents TWO icons
+    let navBook = document.getElementById('navBook');
+    let navUser = document.getElementById('navUser');
 // Get from DOM user navigation
-/**
- * panelUser Id represents two elements with the same Id
- * however logic in template allow to display only one at a time
- * so always dealing with one object
- * @type {HTMLElement}
- */
-let panelUser = document.getElementById('panelUser');
-// Get from DOM book navigation
-let panelBooks = document.getElementById('panelBooks');
+    /**
+     * panelUser Id represents two elements with the same Id
+     * however logic in template allow to display only one at a time
+     * so always dealing with one object
+     * @type {HTMLElement}
+     */
+// Get from DOM user navigation, DOM BUTTON USER
+    let panelUser = document.getElementById('panelUser');
+// Get from DOM book navigation, DOM BUTTON BOOK
+    let panelBooks = document.getElementById('panelBooks');
 
 
-/**
- * User action
- */
-navUser.addEventListener('click', function(){
-    panelUser.classList.toggle('hidden');
-    gsap.from(panelUser, {duration: 1, opacity: 0});
-    // add hidden class to all others menus
-    panelBooks.classList.add('hidden');
+    /**
+     * User action USER BUTTON, DOM BUTTON USER -- ACTION CLICK
+     */
+    navUser.addEventListener('click', function(){
+        panelUser.classList.toggle('hidden');
+        gsap.from(panelUser, {duration: 1, opacity: 0});
+        // add hidden class to all others menus
+        panelBooks.classList.add('hidden');
 
-});
+    });
+    /**
+     * User action BOOK BUTTON, DOM BUTTON BOOK -- ACTION CLICK
+     */
+    navBook.addEventListener('click', function(){
+        panelBooks.classList.toggle('hidden');
+        gsap.from(panelBooks, {duration: 1, opacity: 0});
+        // add hidden class to all others menus
+        panelUser.classList.add('hidden');
+    });
 
-navBook.addEventListener('click', function(){
-    panelBooks.classList.toggle('hidden');
-    gsap.from(panelBooks, {duration: 1, opacity: 0});
-    // add hidden class to all others menus
-    panelUser.classList.add('hidden');
-});
+
+
---
+++ src/Controller/TestController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\BookRepository;
+use App\Service\CategoryExtractor;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController extends AbstractController
+{
+    /**
+     * @Route("/test", name="app_test")
+     */
+    public function index(BookRepository $repository): Response
+    {
+        $raw_data = $repository->getCategories();
+        $categories = new CategoryExtractor($raw_data);
+        $results = $categories->getMainCategories();
+
+        return $this->render('test/test.html.twig', [
+            'categories' => $results,
+        ]);
+    }
+}
