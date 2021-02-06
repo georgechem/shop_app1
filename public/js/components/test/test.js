09e4d18ebb028d5421cb95133ae438b894d1e5a2
georgechem <54766527+georgechem@users.noreply.github.com>
2021-02-06 19:59:27 +0000

    styles fixed

--- public/js/components/test/test.js
+++ public/js/components/test/test.js
@@ -1,14 +1,42 @@
+let container = document.getElementById('div');
 
-let input = document.getElementById('text');
-let submit = document.getElementById('submit')
 
-let message_block = document.getElementById('sent');
 
-submit.addEventListener('click', function(e){
-    e.preventDefault();
-    let element = document.createElement('div');
-    element.classList.add('sent__message');
-    element.innerText = input.value+' ! ';
-    message_block.appendChild(element);
-    input.value = '';
-});
+let dot = document.createElement('div');
+dot.classList.add('dot');
+
+
+
+container.appendChild(dot);
+
+/**
+ * center for DOT inside box
+ * @type {string}
+ * dot.style.top = '62.5px';
+ dot.style.left = '62.5px';
+ */
+function moveRight(){
+    let x = 0;
+    let y = 0;
+    let offset = 62.5;
+    let factor_y = 1;
+    let factor_x = 1;
+    let width = container.offsetWidth - dot.offsetWidth;
+    console.log(width);
+    let animate = setInterval(function(){
+        if(x >= width || x < 0){
+            factor_x = - factor_x;
+            //clearInterval(animate);
+        }
+        dot.style.left = `${x}px`;
+        dot.style.top = `${y+offset}px`;
+        x += 1 * factor_x;
+        y += 1 * factor_y;
+        if (y <= 0 || y > offset){
+            factor_y *= -1;
+        }
+
+
+    }, 1);
+}
+moveRight();
