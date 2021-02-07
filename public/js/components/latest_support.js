6b57f33036fd536b94a6e6b210e5d07a468cdca1
georgechem <54766527+georgechem@users.noreply.github.com>
2021-02-07 18:45:11 +0000

    added latest support JS

--- public/js/components/latest_support.js
+++ public/js/components/latest_support.js
@@ -1,1 +1,26 @@
+let click = new Event('click');
+let previous = 0;
+let acc = [start, total, counter];
+
+
+function moveLatestRight(){
+    let moveLatestRightInterval = setInterval(function(){
+        previous = start;
+        latestRight.dispatchEvent(click);
+        if (previous === start){
+            clearInterval(moveLatestRightInterval);
+            start = acc[0];
+            total = acc[1];
+            counter = acc[2];
+            setTimeout(runLatest, 1500);
+        }
+
+    }, 4000);
+}
+
+
+function runLatest(){
+    setTimeout(moveLatestRight, 1000);
+}
+runLatest();
 
