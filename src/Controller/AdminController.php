ebeaca187ee57d74ff5ee99b18215e8dbb769f90
georgechem <54766527+georgechem@users.noreply.github.com>
2021-02-08 16:12:37 +0000

    start workin on admin pannel

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class AdminController
+ * @package App\Controller
+ * @IsGranted("ROLE_ADMIN")
+ */
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/adminPanel", name="app_admin_panel")
+     */
+    public function index(): Response
+    {
+        return $this->render('admin/index.html.twig', [
+
+        ]);
+    }
+}
